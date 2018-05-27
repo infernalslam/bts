5f8e2e74c8f8c628d2341ee3af1b17fe73468a04
infernalslam <infernal-slam@hotmail.com>
2018-05-28 00:43:19 +0700

    bts btn fix props component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,17 +3,17 @@
   components BTS x Bulma <br>
   <!-- button -->
     <bts-button btsStyle="color: red;" message="BTS"></bts-button>
-    <bts-button message="primary" bts-type="is-primary"></bts-button>
-    <bts-button message="info" bts-type="is-info" :btsOutlined="true"></bts-button>
-    <bts-button message="success" bts-type="is-success" btsSize="medium"></bts-button>
-    <bts-button message="warning" bts-type="is-warning" btsSize="small"></bts-button>
-    <bts-button message="danger" bts-type="is-danger" btsSize="large"></bts-button>
+    <bts-button message="Primary" bts-type="is-primary"></bts-button>
+    <bts-button message="Info" bts-type="is-info" :btsOutlined="true"></bts-button>
+    <bts-button message="Success" bts-type="is-success" btsSize="medium"></bts-button>
+    <bts-button message="Warning" bts-type="is-warning" btsSize="small"></bts-button>
+    <bts-button message="Danger" bts-type="is-danger" btsSize="large"></bts-button>
   <!-- button -->
 </div>
 </template>
 
 <script>
-import BtsButton from './button/index'
+import BtsButton from './button'
 export default {
   components: {
     BtsButton
