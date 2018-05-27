1328d8cafe9821f85177a2d8922f7a9e7f45b4f6
infernalslam <infernal-slam@hotmail.com>
2018-05-28 00:21:52 +0700

    fix code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,11 +3,11 @@
   Hello components BTS x Bulma
   <!-- custom style -->
   <bts-button btsStyle="color: red;" message="BTS"></bts-button>
-  <bts-button message="BTS" bts-type="is-primary"></bts-button>
-  <bts-button message="BTS" bts-type="is-info"></bts-button>
-  <bts-button message="BTS" bts-type="is-success"></bts-button>
-  <bts-button message="BTS" bts-type="is-warning"></bts-button>
-  <bts-button message="BTS" bts-type="is-danger"></bts-button>
+  <bts-button message="primary" bts-type="is-primary"></bts-button>
+  <bts-button message="info" bts-type="is-info"></bts-button>
+  <bts-button message="success" bts-type="is-success"></bts-button>
+  <bts-button message="warning" bts-type="is-warning"></bts-button>
+  <bts-button message="danger" bts-type="is-danger"></bts-button>
 </div>
 </template>
 
--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,7 +1,5 @@
 <template>
-<div>
-  <button class="button" :class="btsType + btsRounded" :style="btsStyle">  {{ message }} </button>
-</div>
+  <div class="button " :class="btsType + btsRounded" :style="btsStyle">  {{ message }} </div>
 </template>
 
 <script>
@@ -13,11 +11,32 @@ export default {
       type: null,
       default: this.btsRounded ? 'is-rounded' : ''
     },
-    btsType: String
+    btsType: {
+      type: String,
+      default: this.btsStyle ? this.btsStyle : ''
+    }
   }
 }
 </script>
 
 <style scoped>
+
+.is-primary {
+  background-color: #2074ff !important;
+}
+ /* optional hover color */
+/* .is-primary:hover {
+  box-shadow: 0 9px 28px -9px #2074ff !important;
+} */
+
+.is-success {
+  background-color: #45c93a !important;
+}
+.is-warning {
+  background-color: #ffba00 !important;
+}
+.is-danger {
+  background-color: #ff4657 !important;
+}
 
 </style>
